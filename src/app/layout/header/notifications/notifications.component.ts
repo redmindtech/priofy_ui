ea33fcf2fd2b2d329d41e447485ef691f3cb792f
Balaparthi2001 <balaparthi06@gmail.com>
2024-04-06 19:08:00 +0530

    bala changes

--- src/app/layout/header/notifications/notifications.component.ts
+++ src/app/layout/header/notifications/notifications.component.ts
@@ -1,7 +1,51 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { NotificationService } from '@app/utils/service/notification.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.css']
 })
-export class NotificationsComponent { }
+export class NotificationsComponent { 
+  private onSubmitInterval: any;
+  private addSubscription: Subscription | undefined;
+  remainingValues: any;
+  constructor(private fb: FormBuilder,  private apiService: NotificationService )
+{}
+
+ngOnInit(): void {
+
+this.setupSubmitInterval();
+}
+setupSubmitInterval() {
+  this.onSubmitInterval = setInterval(() => {
+    console.log('onSubmitInterval: ', this.onSubmitInterval);
+    this.add();
+  }, 5* 1000); // 2 minutes in milliseconds
+}
+
+add() {
+  this.apiService.getnotification().subscribe((response: any) => {
+    console.log(response, 'checking');
+    
+    this.remainingValues = response.result;
+
+    // Iterate through the values and check if any value is not null or empty
+    Object.keys(this.remainingValues).forEach(key => {
+      if (this.remainingValues[key]) {
+        // If the value is not null or empty, patch it
+        
+        console.log('this.remainingValues[key]: ', this.remainingValues[key]);
+      }
+    });
+  });
+}
+
+patchValue(value: any) {
+  // Your patch logic here
+}
+
+
+
+}
---
+++ src/app/utils/service/notification.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  baseUrl: string = "http://localhost:8080/CheckListE";
+
+  constructor(private httpClient: HttpClient) { }
+ // Create new permit data
+ public savenotification(data: any): Observable<any> {
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }),
+  };
+
+  return this.httpClient.post(this.baseUrl+('/ChecklistESave'), data, httpOptions);
+}
+public getnotification(): Observable<any> {
+  return this.httpClient.get(this.baseUrl+('/lastcomment'));
+}
+}
