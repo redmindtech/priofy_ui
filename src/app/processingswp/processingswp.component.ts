7fc56d55541bbffb7c54876d9fbd1a753592f11e
Balaparthi2001 <balaparthi06@gmail.com>
2024-04-17 17:13:31 +0530

    bala

--- src/app/processingswp/processingswp.component.ts
+++ src/app/processingswp/processingswp.component.ts
@@ -8,12 +8,29 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ProcessingswpComponent implements OnInit {
   Safeworkpermit!: FormGroup;
+
+  formattedDate: string;
+  formattedTime: string;
   
   constructor(
     private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    const currentDate = new Date();
+
+// Format the date as needed (e.g., DD/MM/YYYY)
+this.formattedDate = `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}`;
+
+
+// Format the time as needed (e.g., HH:MM:SS)
+this.formattedTime = `${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+
+
+// Combine date and time
+
+
+
     this.formInitialization();
   }
   formInitialization(){
@@ -26,7 +43,21 @@ export class ProcessingswpComponent implements OnInit {
       chemicals_last_contained:['yyy'],
       inspection_required:['t'],
       checklists_Attachments_Required:['rr'],
-     
+      red_tag:['white tag'],
+      reason_for_work:['money'],
+      work_description:['work is Done'],
+      add_tag:['4'],
+      facility_representative_sign:[''],
+      job_representative_sign:[''],
+      employee_number:[''],
+      signature_other_unit_facility_representative:[''],
+      isolating_and_securing:[''],
+      hazards_energies:[''],
+      residual_energy_material:[''],
+      inspection_required_job:[''],
+      inadvertent_operation:[''],
+      special_instructions:['']
+
   })
   }
 }
