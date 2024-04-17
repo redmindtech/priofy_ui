253fcfde84b994925d078a5d641970231332966f
redmindtech <redmindtech.2020@gmail.com>
2024-04-17 20:18:19 +0530

    sudha changa

--- src/app/swpcloseout/swpcloseout.component.ts
+++ src/app/swpcloseout/swpcloseout.component.ts
@@ -14,12 +14,46 @@ export class SwpcloseoutComponent implements OnInit {
     this.formInitialization()
   }
   formInitialization() {
-    this.swpcloseout = this.formBuilder.group({
+    const currentDate = new Date().toISOString().split('T')[0]; // Format: YYYY-MM-DD
+  const currentTime = new Date().toTimeString().split(' ')[0]; 
+  console.log('Current Date:', currentDate);
+  console.log('Current Time:', currentTime);
+  this.swpcloseout = this.formBuilder.group({
       Housekeeping:[null],
       jobandequipment:[null],
       Radioactive:[null],
-      AreaEquipment:[null],
-      closeoutstatus:['Pending']
+      Area_Equipment:[null],
+      closeoutstatus:['Pending'],
+      close_out_status_date:[currentDate],
+      close_out_status_time:[[currentTime]],
+      additionalchecklists:[null],
+      additionalchecklists_date:[currentDate],
+      additionalchecklists_time:[currentTime],
+      Work_Completed:[null],
+      Work_Completed_emp:[null],
+      Work_Completed_date:[currentDate],
+      Work_Completed_time:[currentTime],
+      Additional_Work:[null],
+      Additional_Work_Completed:[null],
+      Additional_Work_Completed_emp:[null],
+      Additional_Work_date:[currentDate],
+      Additional_Work_time:[currentTime],
+      HouseKeeping_Comp_emp:[null],
+      HouseKeeping_Comp:[null],
+      HouseKeeping_Comp_date:[currentDate],
+      HouseKeeping_Comp_time:[currentTime],
+      HouseKeepingCheck:[null],
+      HouseKeeping:[null],
+      HouseKeeping_emp:[null],
+      HouseKeeping_date:[currentDate],
+      HouseKeeping_time:[currentTime],
+      EquipmentTested:[null],
+      Detagged:[null],
+      ReadyforService:[null],
+      EquipmentTested_emp:[null],
+      EquipmentTested_date:[currentDate],
+      EquipmentTested_time:[currentTime],
+
  
     });
   }
