cfb8a43a9be3a852644597d9dac1c2d577ec5433
redmindtech <redmindtech.2020@gmail.com>
2024-05-06 17:16:46 +0530

    sudha changes

--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,115 +1,104 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ActivatedRoute, Router,NavigationEnd  } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
   public sidebarMenuOpened = true;
- // public menu = MENU;
- public menu: any[] = [];
+  // public menu = MENU;
+  public menu: any[] = [];
   currentUser: any;
   username: any;
-  headname:any;
-  screen:any;
-  fullUrl:any;
+  headname: any;
+  screen: any;
+  fullUrl: any;
 
-  constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
-
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
+  
   ngOnInit(): void {
-   
     const storedUser = localStorage.getItem('currentUser');
     this.currentUser = storedUser ? JSON.parse(storedUser) : null;
-   let name= this.currentUser.username
-   console.log(name)
-    this.router.events.subscribe(event => {
+    let name = this.currentUser.username;
+    console.log(name);
+    this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         // Get the full URL
         this.fullUrl = event.url;
         console.log(this.fullUrl);
-        console.log(name)
-        if(name === 'Admin')          {
-          console.log('else')
-            if (this.fullUrl === '/main/maindashboard') {
-              this.screen = "Main Dashboard";
-            } 
-             if (this.fullUrl === '/main/dashboard'){
-              this.screen = "Procedure Catalogs";
-            }
-            if (this.fullUrl === '/main/Admin'){
-              this.screen = "Work Orders";
-            }
-
+        console.log(name);
+        if (name === 'Admin') {
+          console.log('else');
+          if (this.fullUrl === '/main/maindashboard') {
+            this.screen = 'Main Dashboard';
           }
-        if(name=="Operator1" || name=="Operator2" ||name=="Operator3" || name=="Operator4"){
-        if (this.fullUrl === '/main/dashboard') {
-          this.screen = "Dashboard";
-        } 
-         if (this.fullUrl === '/main/home'){
-          this.screen = "My work order";
+          if (this.fullUrl === '/main/dashboard') {
+            this.screen = 'Procedure Catalogs';
+          }
+          if (this.fullUrl === '/main/Admin') {
+            this.screen = 'Work Orders';
+          }
         }
-       
-        
-      }
+        if (
+          name == 'Operator1' ||
+          name == 'Operator2' ||
+          name == 'Operator3' ||
+          name == 'Operator4'
+        ) {
+          if (this.fullUrl === '/main/dashboard') {
+            this.screen = 'Dashboard';
+          }
+          if (this.fullUrl === '/main/home') {
+            this.screen = 'My work order';
+          }
+        }
       }
     });
-    
-   
-   if(name=="Operator1"){
-    this.headname="Process Digitization"
-      this.username="Prakash"
+
+    if (name == 'Operator1') {
+      this.headname = 'Process Digitization';
+      this.username = 'Prakash';
       this.menu = this.getOperator1Menu();
-   }
-   else if(name=="Operator2"){
-    this.headname="Process Digitization"
-    this.username="Bala"
-    this.menu = this.getOperator1Menu();
-   }
-   else if(name=="Operator3"){
-    this.headname="Process Digitization"
-    this.username="Arun"
-    this.menu = this.getOperator1Menu();
-   }
-   else if(name=="Operator4"){
-    this.headname="Process Digitization"
-    this.username="Rangith"
-    this.menu = this.getOperator1Menu();
-   }
-   else if(name=="Admin"){
-    this.headname="Process Digitization"
-    this.username="Shift - Leader Kumar";
-    this.menu = this.getAdminMenu();
-   }
-  else if(name=='Processactivitycoordinator') 
-   {
-    this.headname="Safe Work Permit"
-    this.username="Process Activity Co-Ordinator";
-   }
-   else if(name=='Safeworkpermitissuer' )
-    {
-      this.headname="Safe Work Permit"
-     this.username="Safe Work Permit Issuer";
+    } else if (name == 'Operator2') {
+      this.headname = 'Process Digitization';
+      this.username = 'Bala';
+      this.menu = this.getOperator1Menu();
+    } else if (name == 'Operator3') {
+      this.headname = 'Process Digitization';
+      this.username = 'Arun';
+      this.menu = this.getOperator1Menu();
+    } else if (name == 'Operator4') {
+      this.headname = 'Process Digitization';
+      this.username = 'Rangith';
+      this.menu = this.getOperator1Menu();
+    } else if (name == 'Admin') {
+      this.headname = 'Process Digitization';
+      this.username = 'Shift - Leader Kumar';
+      this.menu = this.getAdminMenu();
+    } else if (name == 'Processactivitycoordinator') {
+      this.headname = 'Safe Work Permit';
+      this.username = 'Process Activity Co-Ordinator';
+    } else if (name == 'Safeworkpermitissuer') {
+      this.headname = 'Safe Work Permit';
+      this.username = 'Safe Work Permit Issuer';
+    } else if (name == 'Facilityrep') {
+      this.headname = 'Safe Work Permit';
+      this.username = 'Facility Representative';
+    } else if (name == 'Energycontrolplan') {
+      this.headname = 'Safe Work Permit';
+      this.username = 'Energy Control Plan';
+    } else if (name == 'Jobrep') {
+      this.headname = 'Safe Work Permit';
+      this.username = 'Job Representative';
     }
-    else if( name=='Facilityrep'){
-      this.headname="Safe Work Permit"
-      this.username="Facility Representative";
-    }
-    else if( name=='Energycontrolplan'){
-      this.headname="Safe Work Permit"
-      this.username="Energy Control Plan";
-    }
-    else if(  name=='Jobrep'){
-      this.headname="Safe Work Permit"
-      this.username="Job Representative";
-    }
-    
-    
 
     document.querySelector('body')?.removeAttribute('class');
-    document.querySelector('body')?.classList.add('sidebar-mini','sidebar-open','layout-fixed');
+    document
+      .querySelector('body')
+      ?.classList.add('sidebar-mini', 'sidebar-open', 'layout-fixed');
   }
 
   toggleMenuSidebar() {
@@ -124,22 +113,20 @@ export class MainComponent implements OnInit {
     }
   }
 
+  // export const MENU = [
+  //   {
+  //     name: 'Dashboard',
+  //     path: ['/main/maindashboard']
+  //   },
+  //   {
+  //     name: 'Shift Leader Dashboard',
+  //     path: ['/main/dashboard']
+  //   },
 
-
-// export const MENU = [
-//   {
-//     name: 'Dashboard',
-//     path: ['/main/maindashboard']
-//   },
-//   {
-//     name: 'Shift Leader Dashboard',
-//     path: ['/main/dashboard']
-//   },
-
-//   {
-//    name: 'Process Initialization',
-//    path: ['/main/Admin']
-//   },
+  //   {
+  //    name: 'Process Initialization',
+  //    path: ['/main/Admin']
+  //   },
   // {
   //   name: 'Administration',
   //   icon: 'fa-users-cog',
@@ -164,18 +151,18 @@ export class MainComponent implements OnInit {
   //     }
   //   ]
   // }
-//];
-private getAdminMenu() {
-  return [
-    { name: 'Dashboard', path: ['/main/maindashboard'] },
-    { name: 'Procedure Catalogs', path: ['/main/dashboard'] },
-    { name: 'Work Orders', path: ['/main/Admin'] }
-  ];
-}
-private getOperator1Menu() {
-  return [
-    { name: 'Dashboard', path: ['/main/dashboard'] },
-    { name: 'My Work Order', path: ['/main/home'] },
-  ];
-}
+  //];
+  private getAdminMenu() {
+    return [
+      { name: 'Dashboard', path: ['/main/maindashboard'] },
+      { name: 'Procedure Catalogs', path: ['/main/dashboard'] },
+      { name: 'Work Orders', path: ['/main/Admin'] },
+    ];
+  }
+  private getOperator1Menu() {
+    return [
+      { name: 'Dashboard', path: ['/main/dashboard'] },
+      { name: 'My Work Order', path: ['/main/home'] },
+    ];
+  }
 }
